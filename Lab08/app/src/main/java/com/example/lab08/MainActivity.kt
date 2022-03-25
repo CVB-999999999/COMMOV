f60d617f67e1c198fb01966f3b30402dc8147b97
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2022-03-25 12:49:21 +0000

    Lab fund 08

---
+++ Lab08/app/src/main/java/com/example/lab08/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.lab08
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.lab08.adapters.TodoAdapter
+import com.example.lab08.models.Todo
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.LocalDate
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        todoAdapter = TodoAdapter(ArrayList())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodo.text.toString()
+
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle, LocalDate.now())
+
+                todoAdapter.addTodo(todo)
+
+                etTodo.text.clear()
+            }
+        }
+    }
+}
---
+++ Lab08/app/src/main/java/com/example/lab08/models/Todo.kt
@@ -0,0 +1,11 @@
+package com.example.lab08.models
+
+import java.time.LocalDate
+
+data class Todo(
+    val title: String,
+    val date: LocalDate
+) {
+
+
+}
