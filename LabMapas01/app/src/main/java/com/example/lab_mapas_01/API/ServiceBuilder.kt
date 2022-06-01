b9fc6abb21d2cdcdd933d3aab69d75e16ef4d195
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2022-06-01 21:57:13 +0100

    Lab Maps 01

---
+++ LabMapas01/app/src/main/java/com/example/lab_mapas_01/API/ServiceBuilder.kt
@@ -0,0 +1,18 @@
+package com.example.lab_mapas_01.API
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceBuilder {
+    private val client = OkHttpClient.Builder().build()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://jsonplaceholder.typicode.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <T> buildService(service: Class<T>): T {
+        return retrofit.create(service)
+    }
+}
